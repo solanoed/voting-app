b96de6377c89f7bded0cb4a213ed090628017c0a
David Solano <dsolano@itnnetworks.com>
2022-11-15 10:54:38 -0500

    ya sirve

--- js/two-step.js
+++ js/two-step.js
@@ -16,21 +16,21 @@ const getEmail = () => {
 let fetchUser = async ({ correo, pass }) => {
   try {
     await axios
-    
+
       .post("https://api-votaciones.vercel.app/login", { correo, pass })
       .then(({ data }) => {
         const { hadRegistered, tokenSession } = data;
         console.log(hadRegistered, tokenSession);
-        if (hadRegistered===false) {
-          console.log("se registro")
+        if (hadRegistered === false) {
+          console.log("se registro");
           next_bool = true;
         } else {
           // next_bool = false;
         }
       })
       .catch(({ response }) => {
-        next_bool = false
-        console.log(next_bool)
+        next_bool = false;
+        console.log(next_bool);
         // const error = response.data.message;
         // errorInter(error)
         console.log(response);
@@ -91,81 +91,73 @@ const errorInter = (error = "No") => {
 
 $(".next").click(function () {
   current_fs = $(this).parent().context.className;
-  if (current_fs=="action-button next") {
-    user_email = getEmail()
-  fetchUser(user_email);
-  if (next_bool) {
-    if (animating) return false;
-    animating = true;
+  if (current_fs == "action-button next") {
+    user_email = getEmail();
+    fetchUser(user_email);
+    if (next_bool) {
+      if (animating) return false;
+      animating = true;
 
-    current_fs = $(this).parent();
-    
-    console.log(current_fs)
-    next_fs = $(this).parent().next();
+      current_fs = $(this).parent();
 
-    //activate next step on progressbar using the index of next_fs
-    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+      console.log(current_fs);
+      next_fs = $(this).parent().next();
 
-    //show the next fieldset
-    next_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate(
-      { opacity: 0 },
-      {
-        step: function (now, mx) {
-          //as the opacity of current_fs reduces to 0 - stored in "now"
-          //1. scale current_fs down to 80%
-          scale = 1 - (1 - now) * 0.2;
-          //2. bring next_fs from the right(50%)
-          left = now * 50 + "%";
-          //3. increase opacity of next_fs to 1 as it moves in
-          opacity = 1 - now;
-          current_fs.css({
-            transform: "scale(" + scale + ")",
-            position: "absolute",
-          });
-          next_fs.css({ left: left, opacity: opacity });
-        },
-        duration: 800,
-        complete: function () {
-          current_fs.hide();
-          animating = false;
-        },
-        //this comes from the custom easing plugin
-        easing: "easeInOutBack",
-      }
-    );
-  } else {
-  }
-  }else if (current_fs=="next action-button"){
-    updateUser()
-    
+      //activate next step on progressbar using the index of next_fs
+      $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+      //show the next fieldset
+      next_fs.show();
+      //hide the current fieldset with style
+      current_fs.animate(
+        { opacity: 0 },
+        {
+          step: function (now, mx) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale current_fs down to 80%
+            scale = 1 - (1 - now) * 0.2;
+            //2. bring next_fs from the right(50%)
+            left = now * 50 + "%";
+            //3. increase opacity of next_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({
+              transform: "scale(" + scale + ")",
+              position: "absolute",
+            });
+            next_fs.css({ left: left, opacity: opacity });
+          },
+          duration: 800,
+          complete: function () {
+            current_fs.hide();
+            animating = false;
+          },
+          //this comes from the custom easing plugin
+          easing: "easeInOutBack",
+        }
+      );
+    } else {
+    }
+  } else if (current_fs == "next action-button") {
+    updateUser();
   }
 
   //hace la llamada a la API para verificar si se encuentra en la base de datos
   //Pero primero la verificacion de que no sea un texto vacío
   // localStorage.removeItem("key");
-  
-  
 });
 const updateUser = async () => {
-try {
+  try {
     await axios
-    
-      .post("https://api-votaciones.vercel.app/login", { correo, pass })
+
+      .post("https://api-votaciones.vercel.app/crearcartera", {
+        hash: document.getElementById("hash").value,
+      })
       .then(({ data }) => {
-        const { hadRegistered, tokenSession } = data;
-        console.log(hadRegistered, tokenSession);
-        if (hadRegistered===false) {
-          console.log("se registro")
-          next_bool = true;
-        } else {
-          // next_bool = false;
-        }
+        document.getElementById("base").style.display = "none";
+        document.getElementById("success").style.display = "block";
+        console.log(data);
       })
       .catch(({ response }) => {
-        next_bool = false
-        console.log(next_bool)
         // const error = response.data.message;
         // errorInter(error)
         console.log(response);
@@ -173,7 +165,8 @@ try {
   } catch (error) {
     console.log(error);
   }
-}
+};
+
 $(".previous").click(function () {
   if (animating) return false;
   animating = true;
